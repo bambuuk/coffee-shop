84a21cc25ea33be729b4782485b2319bd14d501d
Ivan <vanyok.korobka@ukr.net>
2022-11-04 02:45:18 +0200

    realization show coffee item in single coffee page

--- src/pages/SingleCoffeeItemPage/SingleCoffeeItemPage.tsx
+++ src/pages/SingleCoffeeItemPage/SingleCoffeeItemPage.tsx
@@ -1,4 +1,6 @@
 import { useParams, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import OurCoffeeHeader from '../../components/OurCoffeeHeader/OurCoffeeHeader';
 import Footer from '../../components/Footer/Footer';
 import BlackLineWithLogo from '../../components/BlackLineWithLogo/BlackLineWithLogo';
@@ -6,102 +8,82 @@ import coffeeItemImage from '../../resources/img/aromistico-napali.jpg';
 import './singleCoffeeItemPage.scss';
 
 function SingleCoffeeItemPage() {
+  const [coffeeItem, setCoffeeItem] = useState({
+    imgUrl: coffeeItemImage,
+    title: 'loading',
+    price: 'loading',
+    id: 'loading',
+    country: 'loading',
+    description: 'loading',
+    techniclas: {
+      type: 'loading',
+      cuisine: 'loading',
+      format: 'loading',
+      weight: 'loading',
+      flavour: 'loading',
+      roast: 'loading',
+      'country of origin': 'loading',
+      'country of origin of primary ingredient': 'loading',
+    },
+  });
   const idProduct = useParams();
-  console.log(idProduct);
+
+  useEffect(() => {
+    axios.get(`https://62a11ee47b9345bcbe46a4c5.mockapi.io/coffee-list/${idProduct.coffeeId}`)
+      .then((res) => {
+        setCoffeeItem(res.data);
+      });
+  }, []);
+
+  const technicalsArray = Object.entries(coffeeItem.techniclas);
+  const contentTechnicals = technicalsArray.map((item) => {
+    return (
+      <tr className="table_str" key={item[0]}>
+        <td className="leftCell">{item[0][0].toUpperCase() + item[0].slice(1)}</td>
+        <td className="rightCell">{item[1]}</td>
+      </tr>
+    );
+  });
+
+  const {
+    imgUrl, title, price, id, country, description,
+  } = coffeeItem;
+
   return (
     <div className="single-coffee-page">
       <OurCoffeeHeader />
       <div className="container">
         <div className="single-coffee-page__wrapper">
           <div className="single-coffee-page__img">
-            <img src={coffeeItemImage} alt="coffee" />
+            <img src={imgUrl} alt={title} />
           </div>
           <div className="single-coffee-page__info">
             <h2 className="title title_margin-top-9">About it</h2>
             <BlackLineWithLogo />
             <div className="single-coffee-page__country">
               <span className="weight">Country: </span>
-              Brasil
+              {country}
             </div>
             <div className="single-coffee-page__descr">
+              <span className="weight">SKU: </span>
+              {id}
               <span className="weight">Description: </span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel incidunt,
-              eaque totam dolorum quod atque qui vitae error repellat labore sed est
-              aliquid quae aspernatur aliquam. Voluptatum vel error hic.
+              {description}
+            </div>
+            <div className="single-coffee-page__sku">
+              <span className="weight">SKU: </span>
+              {id}
             </div>
             <div className="single-coffee-page__price">
               <span className="weight">Price: </span>
-              <span className="single-coffee-page__price_font-size">16.99$</span>
+              <span className="single-coffee-page__price_font-size">{price}</span>
             </div>
           </div>
           <div className="single-coffee-page__specifications">
             <h2 className="title">Specifications</h2>
             <table className="table">
               <tbody>
-                <tr className="table_str">
-                  <td className="leftCell">Product Dimensions</td>
-                  <td className="rightCell">9 x 8 x 17 cm; 200 Grams</td>
-                </tr>
-                <tr>
-                  <td>Product Name</td>
-                  <td>Ground Coffee</td>
-                </tr>
-                <tr>
-                  <td>Allergen Information</td>
-                  <td>Contains: Gluten Free</td>
-                </tr>
-                <tr>
-                  <td>Weight</td>
-                  <td>200 Grams</td>
-                </tr>
-                <tr>
-                  <td>Units</td>
-                  <td>1.0 gram</td>
-                </tr>
-                <tr>
-                  <td>Solid Net Weight</td>
-                  <td>200 Grams</td>
-                </tr>
-                <tr>
-                  <td>Storage Instructions</td>
-                  <td>Keep in a cool and dry place</td>
-                </tr>
-                <tr>
-                  <td>Country of origin</td>
-                  <td>Italy</td>
-                </tr>
-                <tr>
-                  <td>Country of origin of primary ingredient(s)</td>
-                  <td>Ethiopia</td>
-                </tr>
-                <tr>
-                  <td>Location Produced In of primary ingredient(s)</td>
-                  <td>Italy</td>
-                </tr>
-                <tr>
-                  <td>Brand</td>
-                  <td>AROMISTICO Crafted Selections of Italian Coffee</td>
-                </tr>
-                <tr>
-                  <td>Type</td>
-                  <td>COARSE GROUND</td>
-                </tr>
-                <tr>
-                  <td>Format</td>
-                  <td>Ground</td>
-                </tr>
-                <tr>
-                  <td>Region Produced In</td>
-                  <td>Lombardy</td>
-                </tr>
-                <tr>
-                  <td>Speciality</td>
-                  <td>Natural</td>
-                </tr>
-                <tr>
-                  <td>Caffeine content</td>
-                  <td>Caffeinated</td>
-                </tr>
+                {contentTechnicals}
               </tbody>
             </table>
           </div>
