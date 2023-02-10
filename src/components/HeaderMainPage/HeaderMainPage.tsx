321901b33fdd4058a3729399d681961c88a4b69a
Ivan <vanyok.korobka@ukr.net>
2023-02-10 20:47:27 +0200

    added some responsive layout

--- src/components/HeaderMainPage/HeaderMainPage.tsx
+++ src/components/HeaderMainPage/HeaderMainPage.tsx
@@ -8,15 +8,15 @@ function HeaderMainPage() {
     <header className="header">
       <div className="container">
         <HeaderNavbar />
-        <h1 className="header__title header__title_pd-top111">Everything You Love About Coffee</h1>
+        <h1 className="header__title">Everything You Love About Coffee</h1>
         <div className="line-with-logo">
           <div className="line-with-logo__line line-with-logo__line_white" />
           <img src={beansLogo} className="line-with-logo__logo" alt="beans logo" />
           <div className="line-with-logo__line line-with-logo__line_white" />
         </div>
         <div className="header__subtitle">
-          <span className="header__subtitle__line">We makes every day full of energy and taste</span>
-          <span className="header__subtitle__line">Want to try our beans?</span>
+          <div className="header__subtitle__line">We makes every day full of energy and taste</div>
+          <div className="header__subtitle__line">Want to try our beans?</div>
         </div>
         <div className="more-info">More</div>
       </div>
