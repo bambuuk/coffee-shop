88a5cb126e165fa438b2c3bf027308bf2f699fc3
Ivan <vanyok.korobka@ukr.net>
2023-06-16 00:38:54 +0300

    upd HeaderNavbar component - made it in tsx format

--- src/components/HeaderNavbar/HeaderNavbar.tsx
+++ src/components/HeaderNavbar/HeaderNavbar.tsx
@@ -1,6 +1,4 @@
 import { NavLink } from 'react-router-dom';
-
-// import logo from '../../resources/img/icon_coffe_header_nav.svg';
 import './headerNavbar.scss';
 
 function HeaderNavbar() {
@@ -8,12 +6,10 @@ function HeaderNavbar() {
     <nav className="navbar">
       <ul className="navbar-list">
         <li className="navbar-list__item">
-          {/* <img src={logo} alt="logo" className="navbar__logo" /> */}
           <NavLink 
             to="/"
             style={({ isActive }) => ({ color: isActive ? '#fc7107' : '#fff' })}
             className="navbar-list__link" 
-            href="#empty"
           >
             Coffee house
           </NavLink>
@@ -23,7 +19,6 @@ function HeaderNavbar() {
             to="/our-coffee"
             style={({ isActive }) => ({ color: isActive ? '#fc7107' : '#fff' })}
             className="navbar-list__link" 
-            href="#empty"
           >
             Our coffee
           </NavLink>
@@ -33,7 +28,6 @@ function HeaderNavbar() {
             to="/for-your-pleasure"
             style={({ isActive }) => ({ color: isActive ? '#fc7107' : '#fff' })}
             className="navbar-list__link" 
-            href="#empty"
           >
             For your pleasure
           </NavLink>
