4c4de797505e51ef5a8c4353ab747b9eaa1b952d
Ivan <vanyok.korobka@ukr.net>
2023-12-12 19:34:33 +0200

    upd products in BestProduct components

--- src/components/BestProduct/BestProduct.tsx
+++ src/components/BestProduct/BestProduct.tsx
@@ -7,31 +7,31 @@ function BestProduct() {
         <h2 className="title">Our best</h2>
         <div className="best-product__coffe-list">
           <div className="best-coffee-item">
-            <img 
-              className="best-coffee-item__img" 
-              src="https://i.ebayimg.com/images/g/LoIAAOSwi3Fgw1NI/s-l500.png" 
-              alt="Solimo Coffee Beans 2 kg"
+            <img
+              className="best-coffee-item__img"
+              src="https://i.pinimg.com/originals/8d/2c/1c/8d2c1c42b83d38d8b2ed5d45034eb3e5.jpg"
+              alt="Premium Coarse Coffee for COLD BREW"
             />
-            <div className="best-coffee-item__title">Solimo Coffee Beans 2 kg</div>
-            <div className="best-coffee-item__price">10.73$</div>
+            <div className="best-coffee-item__title">Premium Coarse Coffee for COLD BREW</div>
+            <div className="best-coffee-item__price">20$</div>
           </div>
           <div className="best-coffee-item">
-            <img 
-              className="best-coffee-item__img" 
-              src="https://i.ebayimg.com/thumbs/images/g/gKwAAOSw0Vxipz0Z/s-l300.jpg" 
-              alt=""
+            <img
+              className="best-coffee-item__img"
+              src="https://i.pinimg.com/originals/b0/a0/1c/b0a01c76c8b269e8c2c471f5cf193f04.jpg"
+              alt="Premium Coarse Coffee for COLD BREW"
             />
-            <div className="best-coffee-item__title">Presto Coffee Beans 1 kg</div>
-            <div className="best-coffee-item__price">15.99$</div>
+            <div className="best-coffee-item__title">Premium Coarse Coffee for COLD BREW</div>
+            <div className="best-coffee-item__price">4$</div>
           </div>
           <div className="best-coffee-item">
-            <img 
-              className="best-coffee-item__img" 
-              src="https://m.media-amazon.com/images/I/71TcI6NptNL.jpg" 
-              alt="AROMISTICO Coffee 1 kg"
+            <img
+              className="best-coffee-item__img"
+              src="https://i.pinimg.com/originals/db/f8/ab/dbf8ab3b5368cf013d468711f5fa734b.jpg"
+              alt="Rich Strong Dark Roast Coffee Beans"
             />
-            <div className="best-coffee-item__title">AROMISTICO Coffee 1 kg</div>
-            <div className="best-coffee-item__price">6.99$</div>
+            <div className="best-coffee-item__title">Rich Strong Dark Roast Coffee Beans</div>
+            <div className="best-coffee-item__price">12$</div>
           </div>
         </div>
       </div>
